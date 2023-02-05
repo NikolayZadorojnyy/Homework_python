c8226f5ae5b70971239c42f8a348bcbbfcf4069b
Nikolay Zadorojnyy <znikolay69@gmail.com>
2023-02-05 03:25:54 +0300

    fixes less_2 task_3.py

--- less_2/task_3.py
+++ less_2/task_3.py
@@ -16,13 +16,5 @@
 """
 a = [10, "Hello", 100.0, True, None, {1:'value', 'key':2}, {5,2,3,1,4}, [5,10,15], (5,'program', 1+3j), 1+2j]
 
-print(type(a[0]))
-print(type(a[1]))
-print(type(a[2]))
-print(type(a[3]))
-print(type(a[4]))
-print(type(a[5]))
-print(type(a[6]))
-print(type(a[7]))
-print(type(a[8]))
-print(type(a[9]))
+for el in a:
+    print(type(el))
